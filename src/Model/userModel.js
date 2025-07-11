4d3c328f94e173ff5abfb3ac7f0701c960e78434
YugalTHapa <ytmagar08@gmail.com>
2025-07-11 19:09:05 +0545

    add userModel

---
+++ src/Model/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    email : {
+        type : String,
+        required : true,
+        null : false,
+        unique : true,
+    },
+    password : {
+        type : String,
+        required : true,
+        null : false,
+    },
+    fullName : {
+        type : String,
+        required : true,
+        null : false,
+    },
+    address : {
+        type : String,
+        required : true,
+        null : false,
+    },
+    citizenId : {
+        type : String,
+        required : true,
+        null : false,
+        unique : true,
+    }
+})
